15a415086aab520cacdb6a1eeac9fbd64a9b80b0
julesb-10 <74217433+julesb-10@users.noreply.github.com>
2025-11-27 16:01:09 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -11,19 +11,28 @@ Purpose:
 
 
 -- CREATING TABLES FOR BRONZE LAYER:
+USE DataWarehouse;
 
 -- crm_cust_info:
+/*
+Note: Can also check if tables exist in MS SQL SERVER by using:
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL -- U: User-defined Table
+	DROP TABLE bronze.crm_cust_info;
+
+*/
+
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info(
 	cst_id INT,
-	cst_key VARCHAR(50),
-	cst_firstname VARCHAR(50),
-	cst_lastname VARCHAR(50),
-	cst_marital_status VARCHAR(50),
-	cst_gndr VARCHAR(50),
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
-
+GO
 
 
 -- prd_info:
@@ -31,22 +40,22 @@ CREATE TABLE bronze.crm_cust_info(
 DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
-	prd_key VARCHAR(50),
-	prd_nm VARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
 	prd_cost INT,
-	prd_line VARCHAR(50),
-	prd_start_dt TIMESTAMP,
-	prd_end_dt TIMESTAMP
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 );
-
+GO
 
 
 -- sales details:
 
 DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details(
-	sls_ord_num VARCHAR(50),
-	sls_prd_key VARCHAR(50),
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
 	sls_order_dt INT,
 	sls_ship_dt INT,
@@ -55,26 +64,26 @@ CREATE TABLE bronze.crm_sales_details(
 	sls_quantity INT,
 	sls_price INT
 );
-
+GO
 
 -- Cust_az12:
 
 DROP TABLE IF EXISTS bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
-	cid VARCHAR(50),
+	cid NVARCHAR(50),
 	bdate DATE,
-	gen VARCHAR(50)
+	gen NVARCHAR(50)
 );
-
+GO
 
 -- loc_a101:
 
 DROP TABLE IF EXISTS bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
-	cid VARCHAR(50),
-	cntry VARCHAR(50)
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
 );
-
+GO
 
 -- PX_CAT_G1V2:
 
@@ -85,3 +94,4 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 	subcat VARCHAR(50),
 	maintenance VARCHAR(50)
 );
+GO
